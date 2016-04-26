17eca916d7e1bf2717d598f42521212f986ee6de
sunwoo48 <anatado@hotmail.com>
2016-04-26 00:10:35 -0400

    edit

--- app/app.js
+++ app/app.js
@@ -11,17 +11,6 @@ app.config(function($httpProvider, $urlRouterProvider, $stateProvider){
 	templateUrl: 'sites/partials/main.html',
 	controller: 'mainctrl as ctrl'
 	})
-	.state('gallary',{
-	url: '/gallary',
-	templateUrl: 'sites/partials/gallary.html',
-	controller: 'gallaryctrl as ctrl'
-	})
-	.state('about',{
-	url: '/about',
-	templateUrl: 'sites/partials/about.html',
-	controller: 'aboutctrl as ctrl'
-	})
-
 
 	
 });
--- app/sites/controllers/mainctrl.js
+++ app/sites/controllers/mainctrl.js
@@ -1,59 +1,25 @@
-(function(){
+  (function(){
   'use strict'
 
       angular
         .module('photo')
         .controller('mainctrl', mainctrl);
 
-  function mainctrl($state) {
+  function mainctrl($timeout, $mdSidenav, $log) {
+
     var ctrl = this;
-    ctrl.reopen = reopen;
-    ctrl.$state = $state;
+  
+  ctrl.categories = [
+		{label:'Portrait',value:'Portrait'},
+		{label:'Landscape',value:'Landscape'},
+		{label:'Wedding',value:'Wedding'},
+	];
+	ctrl.category='';
 
 
-if (document.referrer == "http://localhost:80/#/main") {
-  window.location.reload(); 
-
-} 
-
-
-
-    function reopen() {
-        ctrl.$state.go('main');
-        location.reload();
-    }
-    
   }
 })();
 
 
 
 
-
-
-
-
-  // ctrl.addSlide =addSlide;
-  // ctrl.randomize = randomize;
-
-
-
-  // ctrl.active = 0;
-  // var slides = ctrl.slides = [];
-  // var currIndex = 0;
-
-  //  function addSlide() {
-  //   var newWidth = 400 + slides.length + 1;
-  //   slides.push({
-  //     image: 'http://lorempixel.com/' + newWidth + '/150',
-  //   });
-  // };
-
-  // function randomize() {
-  //   var indexes = ctrl.generateIndexesArray();
-  //   ctrl.assignNewIndexesToSlides(indexes);
-  // };
-
-  // for (var i = 0; i < 3; i++) {
-  //   ctrl.addSlide();
-  // }
